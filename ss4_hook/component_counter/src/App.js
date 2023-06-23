4690f616e4930e390776a2d1bee9235b86e76410
Your Name <phanvuankhang@gmail.com>
2023-06-23 11:08:48 +0700

    up

---
+++ ss4_hook/component_counter/src/App.js
@@ -0,0 +1,48 @@
+import './App.css'
+import {useState} from "react";
+
+
+export function useIncrement(addAmount) {
+    const [count, setCount] = useState(0);
+
+    function increase(addAmount) {
+        setCount(count + addAmount)
+    }
+
+    return (
+        [count, increase]
+    )
+}
+
+function Counter1() {
+    const [count, setCount] = useIncrement(1);
+
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={() => setCount(1)}>Add 1</button>
+        </div>
+    );
+}
+
+function Counter2() {
+    const [count, setCount] = useIncrement(5);
+
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={() => setCount(5)}>Add 2</button>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <Counter1/>
+            <Counter2/>
+        </div>
+    );
+}
+
+export default App;
